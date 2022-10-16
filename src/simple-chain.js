b22f8e636052b664df4a93a8a0e448c575939c8b
AlexSerSla <slavckin2009@yandex.ru>
2022-10-16 18:36:49 +0300

    feat: pre done Chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -40,8 +40,10 @@ const { NotImplementedError } = require('../extensions/index.js');
   finishChain() {
     //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    console.log(this.chain.join('~~'));
+    //console.log(this.chain.join('~~'));
+    const resString = this.chain.join('~~');
     this.chain = [];
+    return resString;
   }
 };
 
